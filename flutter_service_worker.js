0de666fab0516f424d540a97f4e37b9db89e28a6
imaachman <imaachman@gmail.com>
2020-04-16 12:32:02 +0530

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -108,7 +108,7 @@ const RESOURCES = {
 "icons/products_app_xxx.png": "908a0adee1a2218dbc6f8921d0522a66",
 "index.html": "0158a19482c1232de4f5bb55750c34ff",
 "/": "0158a19482c1232de4f5bb55750c34ff",
-"main.dart.js": "ad97c25814e30513af5d831b29a47efc",
+"main.dart.js": "e9385f7dc6bbf8ef94f18d68580546c2",
 "manifest.json": "ce9c6bd69cac1b9117d1f84989b8e859",
 "products_app_fav.png": "31634cc5d722be77515d84de81972fbe"
 };
